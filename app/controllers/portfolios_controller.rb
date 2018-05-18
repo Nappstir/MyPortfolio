ffe2e6865ef98e9ca1fe32831015a8a8a7b98245
Siebenhaar <TSiebenhaar@NBK0021031SiebenhaarTravis.local>
2018-05-18 13:05:43 -0600

    integrated new and acreate actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,23 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_item = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:blog).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
